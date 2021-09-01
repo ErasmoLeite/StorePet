c1185716b77b34e8933b8d7b43360ec244b4c66b
Erasmo <erasmo.ldasilva@hotmail.com>
2021-08-31 22:37:03 -0300

    Incluindo contador e log

--- src/test/java/datadriven/UserDD.java
+++ src/test/java/datadriven/UserDD.java
@@ -4,10 +4,9 @@ package datadriven;
 // 3 - Import
 
 import org.json.JSONObject;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.DataProvider;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 import utils.Data;
+import utils.Log;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -15,6 +14,7 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.head;
 import static org.hamcrest.Matchers.is;
 
 
@@ -23,8 +23,11 @@ public class UserDD {
 
         String uri = "https://petstore.swagger.io/v2/user";
         Data data ; // Objeto que representa a classe utils.Date
+        Log  log;   // objeto que representa a classe utils.log
+        int contador = 0; // ajudar a contar o numero de linhas executadas
+        double soma; // somar os valores dos registros ( brincadeira somando o valor das senhas)
 
-        //3.2  - MetÃ³dos e FunÃ§Ãµes
+        //3.2  - Metódos e Funções
 
     @DataProvider
     public Iterator<Object[]> provider() throws IOException {
@@ -32,7 +35,7 @@ public class UserDD {
         //List<String[]> testCases = new ArrayList<>();   // Salvar todos os testes cases
         String[] testCase ;  // Salvar 1 teste de cada vez
         String linha;
-        BufferedReader bufferedReader = new BufferedReader(new FileReader("db/users.csv"));
+        BufferedReader bufferedReader = new BufferedReader(new FileReader("db/userPairwise.csv"));
         while ((linha = bufferedReader.readLine()) != null) {
             testCase = linha.split(",");  // informar o separador das colunas
             testCases.add(testCase);
@@ -41,12 +44,22 @@ public class UserDD {
             return testCases.iterator();
     }
 
-    @BeforeMethod
-    public void setup() {
+
+    @BeforeClass  // Antes da classe que executa os testes
+    public void setup() throws IOException {
         data  = new Data();
+        log = new Log();
+        log.iniciarlog(); // criar o arquivo e escrever a linha de header
 
     }
-        @Test(dataProvider = "provider")
+
+        @AfterClass // Depois que a classe terminar de executar todos os seus testes
+        public void tearDown(){
+            System.out.println("TOTAL DE REGISTROS = " + contador);
+            System.out.println("");
+        }
+
+        @Test(dataProvider = "provider")   // Metodo de teste
         public void incluirUsuario(
                 String id,
                 String username,
@@ -83,8 +96,11 @@ public class UserDD {
                             .extract()
                             .path("message")
                     ;
+            contador += 1;  // somar +1 na variavel contador --> contador = contador + 1
+            System.out.println("O userID é " + userId);
+            System.out.println("Essa é a linha nº " + contador);
 
-            System.out.println("O userID Ã© " + userId);
-        }
+            soma = soma  + Double.parseDouble(password);
+    }
 
     }
